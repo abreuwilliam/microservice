372757eac239af567b8ffe9ba1869d67101b9927
abreuwilliam <https://github.com/abreuwilliam>
2025-01-14 16:21:17 -0300

    project hr work create

---
+++ hrwork/src/main/java/com/devsuperior/hrwork/HrworkApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.hrwork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrworkApplication.class, args);
+	}
+
+}
---
+++ hrwork/src/test/java/com/devsuperior/hrwork/HrworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.hrwork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
