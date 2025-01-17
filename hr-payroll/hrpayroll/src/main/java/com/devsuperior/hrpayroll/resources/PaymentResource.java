98d9cb259321cf7df8b2f467889b62d06e3c6c4c
abreuwilliam <https://github.com/abreuwilliam>
2025-01-17 11:26:31 -0300

    moc

---
+++ hr-payroll/hrpayroll/src/main/java/com/devsuperior/hrpayroll/resources/PaymentResource.java
@@ -0,0 +1,28 @@
+package com.devsuperior.hrpayroll.resources;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.devsuperior.hrpayroll.entities.Payment;
+import com.devsuperior.hrpayroll.services.PaymentService;
+
+@RestController
+@CrossOrigin(origins = "*")
+@RequestMapping(value = "/payments")
+public class PaymentResource {
+    
+    @Autowired
+    private PaymentService service;
+
+    @GetMapping(value = "/{workerID}/days/{days}")
+    public ResponseEntity<Payment> getPayment(@PathVariable Long workerID, @PathVariable Integer days ){
+        Payment Payment = service.getPayment(workerID,days);
+        return ResponseEntity.ok(Payment);
+    }
+
+}
